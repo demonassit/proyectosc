8f93ec908769be547e485e9ada7948e6ccab02fa
Demonassit <demonassit@hotmail.com>
2020-11-11 11:39:52 -0600

    ya quedo solo era un espacio

--- dijstraArbol/dijkstra.cpp
+++ dijstraArbol/dijkstra.cpp
@@ -18,17 +18,13 @@ conexiones:
 
 
 */
-
-
-#include<stdio.h>
-#include<vector>
-#include<queue>
-
+#include <stdio.h>
+#include <vector>
+#include <queue>
 using namespace std;
-
-#define MAX //como maximo 10005 de vertices
-#define Node pair<int, int>  //un nodo de tipo vector donde estamos definiendo <primero, segundo>
-#define INF 1<<30  //definimos un valor que representa la distancia incial hasta con su perior maximo de la arista
+#define MAX 10005 //como maximo 10005 de vertices
+#define Node pair< int , int > //un nodo de tipo vector donde estamos definiendo <primero, segundo>
+#define INF 1<<30 //definimos un valor que representa la distancia incial hasta con su perior maximo de la arista
 
 /*
 como habiamos visto en el pseudocodigo lo primero que se debe de programar
@@ -38,147 +34,114 @@ asi realizar operaciones con su peso, almacernos y despues de ello, realizar
 el algoritmo del mejor camino cuyos pesos sean menores del punta A al B
 */
 
-
 //cola
-struct cpm{
-    bool operador()(const Node &a, const Node &b){
+struct cmp {
+    bool operator() ( const Node &a , const Node &b ) {
         return a.second > b.second;
     }
 };
+vector< Node > ady[ MAX ]; //lista de adyacencia acorde al tamaño definido de memoria
+int distancia[ MAX ];      //distancia entre el vertice inicial al siguiente
+bool visitado[ MAX ];      //que vertices se han visitado
+priority_queue< Node , vector<Node> , cmp > Q; //definir la cola de prioridad, para saber el menor valor que este a tope
+int V;                     //numero de vertices
+int previo[ MAX ];         //para imprimir los caminos
 
-
-vector<Node>ayd[MAX]; //lista de adyacencia acorde al tamaño definido de memoria
-
-int distancia[MAX];  //distancia entre el vertice inicial al siguiente
-
-bool visitado[MAX]; //que vertices se han visitado
-
-//definir la cola de prioridad, para saber el menor valor que este a tope
-priority-queue<Node, vector<Node>, cpm>Q;
-
-int V;  //numero de vertices
-
-int previo[MAX];   //para imprimir los caminos
-
-//vamos a inicializar 
-
+//vamos a inicializar
 void init(){
-
-	for(int i=0; i<=V; i++){
-		distancia[i] = INF; //inicializamos todas las distancias con el valor infinito
-		visitado[i] = false; //iniciaizamos todos los vertices como no visitados
-		previo[i] = -1;  //inicializamos el previo del vertices con -1, para que sepamos que esta afuera del rango
-	}
-
+    for( int i = 0 ; i <= V ; ++i ){
+        distancia[ i ] = INF;  //inicializamos todas las distancias con el valor infinito
+        visitado[ i ] = false; //iniciaizamos todos los vertices como no visitados
+        previo[ i ] = -1;      //inicializamos el previo del vertices con -1, para que sepamos que esta afuera del rango
+    }
 }
 
-
 //vamos a relajarnos wiiii a mimir
-
-void relajacion(int actual, int adyacente, int peso){
-	/*
+void relajacion( int actual , int adyacente , int peso ){
+    /*
 		Si la distancia del origen al vertice actual + el peso de su arista
 		es menor a la distancia del origen al vertices adyacente
 	*/
-	if(distancia[actual] + peso < distancia[adyacente]){
-
-		//relajamos el vertice actualizando la distancia
-		distancia[adyacente] = distancia[actual]+peso;
-		previo[adyacente] = actual  //actualizar el vertice previo
-		Q.push(Node(adyacente, distancia[adyacente])); //la prioridad de la cola de adyacencia	
-
-	}
+    if( distancia[ actual ] + peso < distancia[ adyacente ] ){
+    	//relajamos el vertice actualizando la distancia
+        distancia[ adyacente ] = distancia[ actual ] + peso;  
+        previo[ adyacente ] = actual;                         //actualizar el vertice previo
+        Q.push( Node( adyacente , distancia[ adyacente ] ) ); //la prioridad de la cola de adyacencia	
+    }
 }
-
 
 //vamos a imprimir el camino mas chiquito desde el vertice inicial al final
-
-void print(int destino){
-	if(previo[destino] != -1){
-		//aun no tiene un vertice previo 
-		print(previo[destino]);
-	}//vamos a empezar a explorarlo de forma recursiva
-	print("%d", destino);
+void print( int destino ){
+    if( previo[ destino ] != -1 )
+	//aun no tiene un vertice previo     
+        print( previo[ destino ] );
+		//vamos a empezar a explorarlo de forma recursiva
+    printf("%d " , destino );        
 }
 
-
-//recorrido dijkstra 
-
-void dijkstra(int inicial){
+void dijkstra( int inicial ){
 	//vamos a inicializar los arreglos
-	init();
-	//insertando los vertices a la cola de prioridad
-	Q.push(Node(inicial, 0));  
-	//verificar la distancia inicial a 0
-	distancia[inicial] = 0;
-	
-	int actual, adyacente, peso;
-	
-	//mientras cola no este vacia
-	while(!Q.empty()){
-		//obtenemos de la cola el nodo con el menor peso
-		actual = Q.top().first;
-		//sacarlo del elemento de la cola
-		Q.pop();
-		//si el vertice acutal ya fue visitado entonces sigo sacando elementos de la cola
-		if(visitado[actual]) continue;
-		//marcar el elemento como visitado
-		visitado[actual]=true;
-		
+    init(); 
+    //insertando los vertices a la cola de prioridad
+    Q.push( Node( inicial , 0 ) ); 
+    //verificar la distancia inicial a 0
+	distancia[ inicial ] = 0;      
+    int actual , adyacente , peso;
+    //mientras cola no este vacia
+	while( !Q.empty() ){                   //Mientras cola no este vacia
+        //obtenemos de la cola el nodo con el menor peso
+		actual = Q.top().first;            
+        //sacarlo del elemento de la cola
+		Q.pop();                           
+        //si el vertice acutal ya fue visitado entonces sigo sacando elementos de la cola
+		if( visitado[ actual ] ) continue; 
+        //marcar el elemento como visitado
+		visitado[ actual ] = true;         
 		//vamos a revisar la adyacencia de los vertices referente al actual
-		for(int i = 0; i < ady[actual].size; ++i){
-			//obtener el numero del vertice adyacente
-			adyacente = ady[actual][i].first;
-			//obtener el peso de la arista
-			peso = ady[actual][i].second;
-			
-			//si el vertice adyacente no fue visitado
-			if(!visitado[adyacente]){
-				//realizamos la relajacion
-				relajacion(actual, adyacente, peso);
-			}
-		}
-	}
-	
-	printf("Distancia mas corta iniciado en le Vertice %d\n", inicial);
-	
-	for(int i = 0; i <= V; ++i){
-		printf("Vertice %d, distancia mas corta %d\n ", i, distancia[i]);
-	}
-	
-	
-	puts("\n Impresion del camino mas corto wiiiii ono uwu \n");
-	
-	//aqui es donde el usuario ingresa el destino
-	printf("Ingrese el vertice destino: ");
-	int destino;
-	
-	scanf("%d", &destino);
-	
-	print(destino);
-	
-	printf("\n");
-		
+        for( int i = 0 ; i < ady[ actual ].size() ; ++i ){ 
+            //obtener el numero del vertice adyacente
+			adyacente = ady[ actual ][ i ].first;   
+            //obtener el peso de la arista
+			peso = ady[ actual ][ i ].second;        
+            //si el vertice adyacente no fue visitado
+			if( !visitado[ adyacente ] ){        
+            //realizamos la relajacion
+                relajacion( actual , adyacente , peso ); 
+            }
+        }
+    }
+
+
+    printf( "Distancias mas cortas iniciando en vertice %d\n" , inicial );
+    for( int i = 1 ; i <= V ; ++i ){
+        printf("Vertice %d , distancia mas corta = %d\n" , i , distancia[ i ] );
+    }
+
+    puts("\n Impresion del camino mas corto wiiiii ono uwu \n");
+    //aqui es donde el usuario ingresa el destino
+    printf("Ingrese vertice destino: ");
+    int destino;
+    scanf("%d" , &destino );
+    print( destino );
+    printf("\n");
 }
-
-
 
 //el main
-
 int main(){
-	int E, origen, destino, peso, inicial;
-	scanf("%d %d", &V, &E);
-	while(E--){
-		printf("Ingresa el origen el destino y su peso");
-		scanf("%d %d %d", &origen, &destino, &peso);
-		//si es un grafo dirigido
-		ady[origen].push_back(Node(destino, peso));
-		//para no dirigido
-		ady[destino].push_back(Node(origen, peso));
-	}
-	printf("Ingresa el Vertice inicial");
-	scanf("%d", &inicial);
-	//mando a llamar a mi metodo de dijkstra
-	dijkstra(inicial);
-	return 0;
+    int E , origen, destino , peso , inicial;
+    printf("Ingresa el numero de vertices y de aristas");
+    scanf("%d %d" , &V , &E );
+    while( E-- ){
+    	printf("Ingresa el origen el destino y su peso");
+        scanf("%d %d %d" , &origen , &destino , &peso );
+        //si es un grafo dirigido
+        ady[ origen ].push_back( Node( destino , peso ) ); 
+        //para no dirigido
+        ady[ destino ].push_back( Node( origen , peso ) ); 
+    }
+    printf("Ingrese el vertice inicial: ");
+    scanf("%d" , &inicial );
+    //mando a llamar a mi metodo de dijkstra
+    dijkstra( inicial );
+    return 0;
 }
