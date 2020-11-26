4321de5190ec2a1d526ff7879c4c0bf822242b4e
Demonassit <demonassit@hotmail.com>
2020-11-26 10:31:19 -0600

    metodo burbuja los dos son el mismo

---
+++ burbuja/burbuja01.cpp
@@ -0,0 +1,49 @@
+/*
+vamos a realizar el algoritmo de ordenamiento de burbuja
+
+*/
+
+#include<stdio.h>
+#include<conio.h>
+
+#define TAM 9
+
+int main(){
+    //el arreglo predefinido para ordenar
+    int a[TAM] = {9, 8, 0, 2, 5, 1 ,3, 2, 9};
+
+    int i, pasada, aux;
+
+    printf("Datos en el ordern inicial del arreglo: \n");
+    for(i=0; i<=TAM; i++){
+        printf("%d", a[i]);
+    }
+
+
+    //vamos a realizar un recorrido para la pasada
+    for(pasada=1; pasada<TAM-1; pasada++){
+        for(i=0; i<=TAM-2; i++){
+            //empezar a realizar las comparaciones
+            if(a[i]>a[i+1]){
+                //cuadno sea verdad
+                //intercambio
+                aux = a[i];
+                //printf("%d\n", aux);
+                a[i] = a[i+1];
+                a[i+1] = aux;
+                //printf("%d\n", a[i+1]);
+            }
+        }
+    
+	}
+    
+    printf("\nDatos ordenaos en sentido ascendente son: \n");
+
+    //para poder visualizar los datos otro for
+    for(i=0; i<=TAM-1; i++)
+        printf("%d", a[i]);
+
+    printf("\n");
+
+    return 0;
+}
